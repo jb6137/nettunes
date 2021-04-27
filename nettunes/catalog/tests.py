a10e94b2e2e42e8a092053e28ce11b9518cdf135
jb6137 <jbyook@hotmail.com>
2021-04-27 18:30:31 -0400

    Add initial catalog page templates, views, and tests

--- nettunes/catalog/tests.py
+++ nettunes/catalog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ViewTests(TestCase):
+    def test_catalog_status_code(self):
+        url = reverse('catalog')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_about_status_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
--- nettunes/catalog/views.py
+++ nettunes/catalog/views.py
@@ -1,3 +1,10 @@
+from catalog.models import Record
 from django.shortcuts import render
 
-# Create your views here.
+
+def about(request):
+    return render(request, 'about.html')
+
+def catalog(request):
+    records = Record.objects.all()
+    return render(request, 'catalog.html', {'records': records})
