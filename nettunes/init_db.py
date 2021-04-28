e597319c057f59941a21cc5ea380d6950b429fc3
jb6137 <jbyook@hotmail.com>
2021-04-28 08:05:58 -0400

    add script to initialize db for manual testing

---
+++ nettunes/init_db.py
@@ -0,0 +1,28 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nettunes.settings")
+
+import django
+django.setup()
+
+from catalog.models import Record
+from django.contrib.auth.models import User
+
+record_data = [('Abbey Road',3,3),('Nevermind',2,2),('Purple Rain',2,2), ('Thriller',2,2), ('The Dark Side of the Moon',1,1), \
+           ('The Wall',2,2),('The Slim Shady LP',1,1),('Lemonade',1,1),('21',1,1),('Scorpion',1,1), \
+           ('My Beautiful Dark Twisted Fantasy',2,2), ('Fantasia',1,1)]
+
+user_data = [('Alice','Alice.Test.2021@gmail.com'), ('Bob','Robert.Test.2021@gmail.com'), ('Charlie','Charles.DJ.Test.2021@gmail.com')]
+
+for entry in record_data:
+    title = entry[0]
+    owned = entry[1]
+    available = entry[2]
+    record = Record(name=title, num_owned=owned, num_available=available)
+    record.save()
+
+for entry in user_data:
+    name = entry[0]
+    email = entry[1]
+    user = User(username=name, email=email)
+    user.save()
+
